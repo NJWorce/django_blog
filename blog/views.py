d412bd503070d6f672b650649641ca5ee13cb432
Nathaniel Worcester <nathaniel.worcester@gmail.com>
2019-03-08 17:01:03 -0600

    added url routing for the blog app, configured app settings and rendered home template using dummy data'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        'author': 'NateW',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': 'March 8, 2019'
+    },
+    {
+    'author': 'Bob Jones',
+    'title': 'Blog Post 2',
+    'content': 'This is the second blog post',
+    'date_posted': 'March 7, 2019'
+    }
+
+
+]
+
+
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+
+	return render(request, 'blog/home.html', context)
+
+
+def about(request):
+	return render(request, 'blog/about.html')
