5ad03573b6c1ee6c3ddbacfa821c698a15f9fc0f
Nathaniel Worcester <nathaniel.worcester@gmail.com>
2019-03-11 20:39:57 -0500

    Added migrations and database config as well as Post model

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-    {
-        'author': 'NateW',
-        'title': 'Blog Post 1',
-        'content': 'This is my first blog post',
-        'date_posted': 'March 8, 2019'
-    },
-    {
-    'author': 'Bob Jones',
-    'title': 'Blog Post 2',
-    'content': 'This is the second blog post',
-    'date_posted': 'March 7, 2019'
-    }
 
 
-]
 
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 
 	return render(request, 'blog/home.html', context)
